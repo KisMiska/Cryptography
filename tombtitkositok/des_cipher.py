60136be5d06d1a21be3e4e0e4bf6aa41c2c52486
KisMiska <galicaervin@gmail.com>
2025-11-08 19:16:44 +0200

    des integration

---
+++ tombtitkositok/des_cipher.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import DES as CryptoDES
+
+
+def des_block_encrypt(block, key):
+
+    if len(block) != 8:
+        raise ValueError(f"DES requires 8-byte blocks")
+    if len(key) != 8:
+        raise ValueError(f"DES requires 8-byte key")
+    
+    cipher = CryptoDES.new(key, CryptoDES.MODE_ECB)
+    return cipher.encrypt(block)
+
+
+def des_block_decrypt(block, key):
+    if len(block) != 8:
+        raise ValueError(f"DES requires 8-byte blocks")
+    if len(key) != 8:
+        raise ValueError(f"DES requires 8-byte key")
+    
+    cipher = CryptoDES.new(key, CryptoDES.MODE_ECB)
+    return cipher.decrypt(block)
